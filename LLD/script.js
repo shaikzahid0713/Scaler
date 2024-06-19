e69267c3c30c89e50561a4c44acbba2b86add151
Shaik Zahid <shaikzahid0713@gmail.com>
2024-06-19 17:14:57 +0000

    Var, Let and Const

--- LLD/script.js
+++ LLD/script.js
@@ -1,8 +1,50 @@
-var a = 5;
-var b = 10;
-console.log(a + b);
+// Variables in Javascript
+
+// Memory space in the system to store values
+// Dynamically Typed Language or Loosely Typed Language
+// 1. Var
+
+var a = true;
+console.log("a ->", a);
+
+a = 5;
+console.log("a ->", a);
+
+a = 'Hi';
+console.log("a ->", a);
 
 
-let x = 5;
-let y = 15;
-console.log(y - x);
+// Drawback of Var : It allow redeclaration of a variable which cannot define its origin
+// This is why "Let" and "Const" are used since they does not allow redeclaration
+
+
+
+// With "Let", Redeclaration is not possible but Reinitialization can be done
+let city = 'Hyderabad';
+console.log("city ->", city);
+
+
+// Try uncommenting this -> Redeclaration (Will throw error)
+// let city = "Vijayawada"; 
+// console.log("city ->", city);
+
+city = "Vijayawada"  // Reinitialization
+console.log("city ->", city);
+
+
+
+// With "Const", Redeclaration and Reinitialization is not possible
+const pi = 3.14;
+console.log("PI -> ", pi);
+
+
+// Uncomment this and execute code.
+// pi = 5;
+// console.log("PI -> ", pi);
+
+
+
+
+
+
+
